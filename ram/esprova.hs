a30b941a8a247be5dc4c067b3c17dd3d74b93cfe
Perro2110 <perrottamarco2011@gmail.com>
2026-05-20 16:45:54 +0200

    Refactor dominance logic and update output data format

--- ram/esprova.hs
+++ ram/esprova.hs
@@ -2,7 +2,7 @@ primi ws     = take 3 ws
 rimanenti ws = tail (tail (tail ws))
 
 
-tresplit [] = []
+tresplit [] = []
 tresplit ws = primi ws : tresplit (rimanenti ws)
 
 
@@ -11,14 +11,13 @@ prendicosto tripletta  =  read ( tripletta !! 2 )  :: Int
 
 
 dominata _ [] = False
-dominata xs (h:ts) |
-                   | (prenditempo xs < prenditempo h) && (prendicosto xs < prendicosto h) = True
-                   | otherwise                                                            = dominata xs ts
+dominata xs (h:ts)
+  | xs == h                                                              = dominata xs ts
+  | (prenditempo h < prenditempo xs) && (prendicosto h < prendicosto xs) = True
+  | otherwise                                                            = dominata xs ts
 
 
-dominantiunici [] = []
-dominantiunici (h:ts) | not (dominata h ts)             = h : dominantiunici ts
-                      | otherwise                       = dominantiunici ts
+dominantiunici xs = [h | h <- xs, not (dominata h xs)]
 
 main = do 
     inpStr <- readFile "memory.txt"
@@ -26,9 +25,3 @@ main = do
     let diverseram = tresplit ws
     let output = unlines (map show (dominantiunici diverseram))
     writeFile "output.txt" output
-
-
-
-
-
-
