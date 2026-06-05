594240eb4b944157eaf640fabc9cdb655079aab7
Perro2110 <perrottamarco2011@gmail.com>
2026-06-05 10:59:11 +0200

    Add RAM data structure and parsing logic
    - Introduced a new data type `Ram` with fields for constructor name, access time, and cost in dollars.
    - Implemented a parser function to convert string representations of RAM data into `Ram` objects.
    - Added a function to determine if one RAM object dominates another based on access time and cost.
    - Created a function to filter out RAMs that are dominated by others.
    - Implemented a string conversion function for `Ram` objects for easy output.
    - Added a main function to read RAM data from a file, process it, and print the non-dominated RAMs.
    - Included a sample RAM data file `ram.txt` with various RAM specifications.

---
+++ SIRAM/ram.hs
@@ -0,0 +1,25 @@
+data Ram = Ram{costruttore :: String, tempoAccesso :: Int, costoInDollari :: Int}deriving (Show,Eq)
+
+parse str = 
+    let [a,b,c] = words str 
+    in Ram a (read b :: Int) (read c ::Int)
+
+a_domminaSu_b a b 
+                    | (tempoAccesso a) <=  (tempoAccesso b) &&  (costoInDollari a) <  (costoInDollari b)    = True 
+                    | (costoInDollari a) <=  (costoInDollari b)  &&  (tempoAccesso a) <  (tempoAccesso b)  = True 
+                    | otherwise                                 = False 
+
+vienemaidominato ram rams = (or (map (\r -> a_domminaSu_b r ram) rams))
+
+prendirammaidominate rams = [r | r <- rams, not(vienemaidominato r rams)]
+
+tostring ram = (costruttore ram)++" "++(show (tempoAccesso ram))++" "++(show (costoInDollari ram))++"\n"
+
+main = do 
+    inpStr <- readFile "ram.txt"
+    print inpStr
+    let liness = lines inpStr
+    let rams   = map parse liness
+    let o = prendirammaidominate rams 
+    let outStr = map tostring o
+    putStrLn (unwords outStr)
