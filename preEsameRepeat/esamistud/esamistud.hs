92e5e61a9ec7be68df4a89243c7afc4578d3f4dd
Perro2110 <perrottamarco2011@gmail.com>
2026-06-08 14:17:47 +0200

    Add initial implementation for exam data processing
    - Created a new file `coorte.txt` to store exam results for students.
    - Implemented Haskell module `esamistud.hs` to parse exam data and calculate statistics.
    - Added functions to filter exams by student name, calculate date differences, and aggregate scores.
    - Included functionality to display results for individual students and overall averages for each course.

---
+++ preEsameRepeat/esamistud/esamistud.hs
@@ -0,0 +1,62 @@
+data InfoEsam = InfoEsam{nomecorso :: String, matricolastud :: Int, giorniimm :: Int, cognomestudente :: String} deriving (Show)
+
+data InfoDatoNome = InfoDatoNome {corso :: String, numP :: Int, somVoti ::Int}deriving (Show)
+
+parse str = let [a,b,c,d] = words str 
+            in InfoEsam a (read b :: Int) (read c :: Int) d
+
+prendiesamidiX storico nomeX = [x | x <-storico, (cognomestudente x) ==  nomeX]
+
+calcoladata p pn = (giorniimm pn) - (giorniimm p)
+
+newt (n2,v2) (n1,v1) = (n2,(v2-v1))
+
+_core [] th = []
+_core (t:ts) th 
+            | t == th   = t:_core ts t 
+            | otherwise = (newt t th): _core ts t 
+
+core t = _core t (head t)
+
+uniq l = foldr (\x acc -> if (elem x acc) then acc else x:acc) [] l
+flat l = foldr (++) [] l
+
+prendituttinome esami nome = [t | t <- esami, fst t == nome]
+
+
+sommi l = foldr(\(n,x) acc -> ((fst acc) + x,(snd acc)+1)) (0,0) l
+
+foo l = map (\x -> (read(show (fst x)) :: Float)/(read(show (snd x)) :: Float)) l 
+
+parsestr1 tt = foldr (\(x,y) acc -> (x ++ " "++ (show y) ++ "\n" ++ acc)) [] tt
+parsestr2 tt = foldr (\(x,y) acc -> (x ++ " "++ (show y) ++ "\n" ++ acc)) [] tt
+
+main = do 
+    inpStr <- readFile "coorte.txt"
+    print "================="
+    print "         es 1    "
+    print "================="
+    print "Inserire cognome idiota: "
+    let liness = lines inpStr
+    let storicoEsami = map parse liness
+    nomepersona <- getLine 
+    let esmidip = prendiesamidiX storicoEsami nomepersona
+    let t = zip (map nomecorso esmidip) (map giorniimm esmidip)    
+    let tt = core t
+    putStrLn (parsestr1 tt)
+
+    print "================="
+    print "         es 2    "
+    print "================="
+
+    let esami = map (\s -> InfoDatoNome s 0 0) (uniq (map nomecorso storicoEsami))
+    let nomipersone = uniq (map cognomestudente storicoEsami)
+
+    let tuttiesamievalutati = flat (map (\n -> core (zip (map nomecorso (prendiesamidiX storicoEsami n)) (map giorniimm (prendiesamidiX storicoEsami n)))) nomipersone)
+    --print tuttiesamievalutati
+    --print (prendituttinome tuttiesamievalutati "Geometria")
+    let out = (zip (map corso esami) (map (\nome -> (sommi (prendituttinome tuttiesamievalutati nome))) (map corso esami)))
+    let tout = map (\(x,y) -> (x,(read(show (fst y)) :: Float)/(read(show (snd y)) :: Float)) ) out
+    putStrLn (parsestr2 tout)
+  
+
