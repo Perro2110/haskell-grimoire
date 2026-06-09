132dfafcf72dd91ac9491eecb18a01630892ce16
Perro2110 <perrottamarco2011@gmail.com>
2026-06-09 13:59:35 +0200

    Add genoma exercise with parsing logic and example data

---
+++ preEsameRepeat/genoma/genoma.hs
@@ -0,0 +1,20 @@
+data Campione = Campione{identificativo ::String, componenti :: String}deriving (Show,Eq)
+
+parse str = let [a,b] = words str 
+            in Campione a b
+
+
+
+
+calcolo str = (read (show ((foldr(\c acc-> if elem c "GC" then acc+1 else acc) 0 str))) :: Float)/(read (show(length str)) :: Float)
+
+eval genoma = identificativo genoma ++" "++ (show (calcolo (componenti genoma)*100))++"%\n"
+
+main = do
+    inpStr <- readFile "genoma.txt"
+    let liness = lines inpStr
+    let campioni = map parse liness
+    print campioni
+    print "============"
+    let evals = map eval campioni 
+    putStrLn (unwords evals)
